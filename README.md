de5e9a1d24a08f0a659363b546b4e718c0865e50
hhiroki <gyouzakuuka@gmail.com>
2021-10-16 12:26:18 +0900

    axumを入れてhello_worldを出すようにした

---
+++ README.md
@@ -0,0 +1,14 @@
+# axum_sample
+
+## setup
+```shell
+$ docker-compose build
+$ docker-compose up
+```
+
+## usage
+```shell
+# hello_world
+$ curl http://localhost:3001
+> Hello, World!
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{handler::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run it with hyper on localhost:3001
+    axum::Server::bind(&"0.0.0.0:3001".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
